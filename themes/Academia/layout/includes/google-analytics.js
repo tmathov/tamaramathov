a023660b59b34f254f5451692db9ba001445f601
Mathias Gatti <mathigatti@gmail.com>
2022-09-09 11:00:50 -0300

    Delete google-analytics.js

--- themes/Academia/layout/includes/google-analytics.js
+++
@@ -1,7 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-W7V7R0M01C"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-W7V7R0M01C');
-</script>
